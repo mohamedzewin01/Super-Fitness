2cb3115595390e56ea67e16b7a28ae11100506eb
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-02-16 23:47:04 +0200

    refactor: clean up unused dialog imports and improve code readability

--- lib/features/auth/presentation/widgets/block_consumer_for_otp.dart
+++ lib/features/auth/presentation/widgets/block_consumer_for_otp.dart
@@ -1,7 +1,5 @@
 import '../../../../core/resources/app_constants.dart';
 import '../../../../core/widgets/custom_dialog.dart';
-import '../../../../core/widgets/show_error_dialog.dart';
-import '../../../../core/widgets/show_loading_dialog.dart';
 import '../pages/reset_password_view.dart';
 import '../view_model/verify_password_view_model/verify_password_cubit.dart';
 import '../view_model/verify_password_view_model/verify_password_state.dart';
@@ -10,12 +8,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/utils/utilss.dart';
 
 class BlocConsumerForOtpVerificationPage extends StatelessWidget {
-  final ValueChanged<bool> onCodeInvalid;// Callback for invalid code
+  final ValueChanged<bool> onCodeInvalid; // Callback for invalid code
   final String email;
 
   const BlocConsumerForOtpVerificationPage({
     super.key,
-    required this.onCodeInvalid,// Required callback
+    required this.onCodeInvalid, // Required callback
     required this.email,
   });
 
@@ -24,22 +22,23 @@ class BlocConsumerForOtpVerificationPage extends StatelessWidget {
     return BlocListener<VerifyPasswordViewModel, VerifyPasswordState>(
         listener: (context, state) {
           if (state is LoadingVerifyPasswordState) {
-           // showLoadingDialog(context);
+            // showLoadingDialog(context);
             CustomDialog.showLoadingDialog(context);
           } else if (state is ErrorVerifyPasswordState) {
             var message = extractErrorMessage(state.exception);
             Navigator.of(context).pop(); // Close loading dialog
             //showErrorDialog(context, message);
-            CustomDialog.showErrorDialog(context,message: message);
+            CustomDialog.showErrorDialog(context, message: message);
           } else if (state is SuccessVerifyPasswordState) {
             Navigator.of(context).pop();
-            Future.delayed(Duration(milliseconds: AppConstants.millisecondsDelayed), () {
+            Future.delayed(
+                Duration(milliseconds: AppConstants.millisecondsDelayed), () {
+              if (!context.mounted) return;
               Navigator.of(context).pushReplacement(MaterialPageRoute(
                 builder: (context) => const ResetPasswordView(),
                 settings: RouteSettings(arguments: email),
               ));
             });
-
           }
         },
         listenWhen: (previous, current) {
